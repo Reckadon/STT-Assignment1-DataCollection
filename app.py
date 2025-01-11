045929704946e0613924b59702bc83d7f79c90b1
Romit Mohane <69901221+Reckadon@users.noreply.github.com>
2025-01-12 00:57:35 +0530

    added opentelemetry metrics

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import json
 import os
+import time
 from flask import Flask, render_template, request, redirect, url_for, flash
 from opentelemetry import trace
 from opentelemetry.sdk.resources import Resource
@@ -7,6 +8,9 @@ from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor, ConsoleSpanExporter
 from opentelemetry.exporter.jaeger.thrift import JaegerExporter
 from opentelemetry.trace import SpanKind, StatusCode
+from opentelemetry import metrics
+from opentelemetry.sdk.metrics import MeterProvider
+from opentelemetry.sdk.metrics.export import (ConsoleMetricExporter, PeriodicExportingMetricReader)
 
 # Flask App Initialization
 app = Flask(__name__)
@@ -23,16 +27,22 @@ jaeger_exporter = JaegerExporter(
 )
 span_processor = BatchSpanProcessor(jaeger_exporter)
 trace.get_tracer_provider().add_span_processor(span_processor)
-
-# Instrument Flask with OpenTelemetry
-# FlaskInstrumentor().instrument_app(app)
-
-# # Configure OpenTelemetry
-# trace.set_tracer_provider(TracerProvider())
-# tracer = trace.get_tracer(__name__)
 console_exporter = ConsoleSpanExporter()
-trace.get_tracer_provider().add_span_processor(BatchSpanProcessor(console_exporter))
+# trace.get_tracer_provider().add_span_processor(BatchSpanProcessor(console_exporter))
 
+# Setting up metrics - counters
+metric_reader = PeriodicExportingMetricReader(ConsoleMetricExporter(), export_interval_millis=5000)
+provider = MeterProvider(metric_readers=[metric_reader])
+# Sets the global default meter provider
+metrics.set_meter_provider(provider)
+# Creates a meter from the global meter provider
+meter = metrics.get_meter("main.meter")
+
+new_course_counter = meter.create_counter("new_course_counter", unit="requests", description="Number of new courses added")
+error_counter = meter.create_counter("error_counter", unit="requests", description="Number of errors")
+page_access_counter = meter.create_counter("page_access_counter", unit="requests", description="Number of page accesses")
+
+timing_histogram = meter.create_histogram("timing_histogram", unit="ns", description="Request timing histogram")
 
 # Utility Functions
 def load_courses():
@@ -52,6 +62,7 @@ def save_courses(data):
     courses.append(data)  # Append the new course
     with open(COURSE_FILE, 'w') as file:
         json.dump(courses, file, indent=4)
+        new_course_counter.add(1)
 
 def create_span(name, Kind=SpanKind.SERVER):
     return tracer.start_as_current_span(name, kind=Kind)
@@ -71,6 +82,7 @@ def index():
         span.set_attribute("http.status_code", 200)
         span.set_status(StatusCode.OK)
         response = render_template('index.html')
+        page_access_counter.add(1, {"page": "index"})
         return response, 200
 
 
@@ -78,6 +90,7 @@ def index():
 @app.route('/catalog')
 def course_catalog():
     with create_span("catalog-span") as span:
+        s_time = time.time_ns()
         span_attributes(span)
         try:
             courses = load_courses()
@@ -85,6 +98,7 @@ def course_catalog():
             span.set_status(StatusCode.ERROR)
             span.add_event("Unable to load course data. Error: " + str(e))
             span.set_attribute("http.status_code", 505)
+            error_counter.add(1)
             return redirect(url_for('index'))
 
         course_names = ", ".join(course['code'] + ' ' + course['name'] for course in courses)
@@ -94,6 +108,9 @@ def course_catalog():
         span.add_event("Rendered Course Catalog")
         span.set_status(StatusCode.OK)
         span.set_attribute("http.status_code", 200)
+        t_time = time.time_ns() - s_time
+        timing_histogram.record(t_time, {"page": "catalog"})
+        page_access_counter.add(1, {"page": "catalog"})
         return response, 200
 
 
@@ -102,6 +119,7 @@ def add_course():
     with create_span("add-course-span") as span:
         span_attributes(span)
         if request.method == 'POST':
+            s_time = time.time_ns()
             course = {
                 'code': request.form['code'],
                 'name': request.form['name'],
@@ -122,12 +140,16 @@ def add_course():
                 span.add_event("Malformed Form Submissions")
                 span.set_status(StatusCode.ERROR)
                 span.set_attribute("http.status_code", 400)
+                error_counter.add(1)
                 return render_template('add_course.html')
 
             span.add_event("Submissions Validated Successfully")
             save_courses(course)
             span.add_event("New Course Added: " + course['code'] + ' ' + course['name'])
             flash(f"Course '{course['name']}' added successfully!", "success")
+            t_time = time.time_ns() - s_time
+            timing_histogram.record(t_time, {"page": "add-course"})
+            new_course_counter.add(1)
 
             span.set_status(StatusCode.OK)
             span.set_attribute("http.status_code", 200)
