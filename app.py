dd90cd6a2e29c47f3092d346e3e0307898fd9b77
23110281 <23110281@iitgn.ac.in>
2025-01-14 23:22:54 +0530

    added comments

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+###############################
+## Import Required Libraries ##
+###############################
 import json
 import logging
 import os
@@ -13,41 +16,50 @@ from opentelemetry import metrics
 from opentelemetry.sdk.metrics import MeterProvider
 from opentelemetry.sdk.metrics.export import (ConsoleMetricExporter, PeriodicExportingMetricReader)
 
-# Flask App Initialization
+# Flask setup
 app = Flask(__name__)
 app.secret_key = 'secret'
 COURSE_FILE = 'course_catalog.json'
 
-# OpenTelemetry Setup
+
+#########################
+## OpenTelemetry Setup ##
+#########################
 resource = Resource.create({"service.name": "course-catalog-service"})
 trace.set_tracer_provider(TracerProvider(resource=resource))
 tracer = trace.get_tracer(__name__)
+
+# Jaeger Exporter
 jaeger_exporter = JaegerExporter(
     agent_host_name="localhost",
     agent_port=6831,
 )
 span_processor = BatchSpanProcessor(jaeger_exporter)
 trace.get_tracer_provider().add_span_processor(span_processor)
+
+# Console Exporter
 console_exporter = ConsoleSpanExporter()
 # trace.get_tracer_provider().add_span_processor(BatchSpanProcessor(console_exporter))
 
-# Setting up metrics - counters
+# Metrics Configuration
 metric_reader = PeriodicExportingMetricReader(ConsoleMetricExporter(), export_interval_millis=5000)
 provider = MeterProvider(metric_readers=[metric_reader])
-# Sets the global default meter provider
 metrics.set_meter_provider(provider)
-# Creates a meter from the global meter provider
 meter = metrics.get_meter("main.meter")
 
+# Defining Metrics
 new_course_counter = meter.create_counter("new_course_counter", unit="requests", description="Number of new courses added")
 error_counter = meter.create_counter("error_counter", unit="requests", description="Number of errors")
 page_access_counter = meter.create_counter("page_access_counter", unit="requests", description="Number of page accesses")
-
 timing_histogram = meter.create_histogram("timing_histogram", unit="ns", description="Request timing histogram")
 
-# python structured logging
+
+##############################
+## Structured Logging Setup ##
+##############################
 class JsonFormatter(logging.Formatter):
     def format(self, record):
+        """format log messages as JSON"""
         log_entry = {
             'timestamp': self.formatTime(record),
             'level': record.levelname,
@@ -58,15 +70,19 @@ class JsonFormatter(logging.Formatter):
         }
         return json.dumps(log_entry, indent=4)
 
+# Configure Logging
 handler = logging.FileHandler('application.log')
 handler.setFormatter(JsonFormatter())
 logger = logging.getLogger('Custom JSON Logger')
 logger.setLevel(logging.INFO)
 logger.addHandler(handler)
 
-# Utility Functions
+
+#######################
+## Utility Functions ##
+#######################
 def load_courses():
-    """Load courses from the JSON file."""
+    """load courses from JSON file"""
     if not os.path.exists(COURSE_FILE):
         return []  # Return an empty list if the file doesn't exist
     with open(COURSE_FILE, 'r') as file:
@@ -77,29 +93,37 @@ def load_courses():
 
 
 def save_courses(data):
-    """Save new course data to the JSON file."""
+    """save the course data to JSON file"""
     courses = load_courses()  # Load existing courses
     courses.append(data)  # Append the new course
     with open(COURSE_FILE, 'w') as file:
         json.dump(courses, file, indent=4)
         new_course_counter.add(1)
 
+
 def create_span(name, Kind=SpanKind.SERVER):
+    """helper function for creating a tracing span"""
     return tracer.start_as_current_span(name, kind=Kind)
 
+
 def span_attributes(span):
+    """add standard attributes to the tracing span"""
     span.set_attribute("http.method", request.method)
     span.set_attribute("http.url", request.url)
     span.set_attribute("peer.ip", request.remote_addr)
     span.set_attribute("http.user_agent", request.headers.get('User-Agent', 'Unknown'))
 
-# Routes
+
+
+##################
+## Flask Routes ##
+##################
 @app.route('/')
 def index():
+    """renders the index page"""
     with create_span("index-span") as span:
         span_attributes(span)
         span.add_event("Rendered Course Information Index Page")
-        span.set_attribute("http.status_code", 200)
         span.set_status(StatusCode.OK)
         logger.info("Page Rendered: Index")
         response = render_template('index.html')
@@ -107,9 +131,9 @@ def index():
         return response, 200
 
 
-
 @app.route('/catalog')
 def course_catalog():
+    """displays the course catalog"""
     with create_span("catalog-span") as span:
         s_time = time.time_ns()
         span_attributes(span)
@@ -117,20 +141,13 @@ def course_catalog():
             courses = load_courses()
         except Exception as e:
             span.set_status(StatusCode.ERROR)
-            span.add_event("Unable to load course data. Error: " + str(e))
-            span.set_attribute("http.status_code", 505)
+            span.add_event("Error loading course data: " + str(e))
             error_counter.add(1)
             return redirect(url_for('index'))
 
-        course_names = ", ".join(course['code'] + ' ' + course['name'] for course in courses)
-        span.add_event(f"Loaded Courses named: {course_names}")
-
         response = render_template('course_catalog.html', courses=courses)
-        span.add_event("Rendered Course Catalog")
         span.set_status(StatusCode.OK)
-        span.set_attribute("http.status_code", 200)
-        t_time = time.time_ns() - s_time
-        timing_histogram.record(t_time, {"page": "catalog"})
+        timing_histogram.record(time.time_ns() - s_time, {"page": "catalog"})
         page_access_counter.add(1, {"page": "catalog"})
         logger.info("Page Rendered: Course Catalog")
         return response, 200
@@ -138,78 +155,41 @@ def course_catalog():
 
 @app.route('/add_course', methods=['GET', 'POST'])
 def add_course():
+    """adds a new course to the catalog"""
     with create_span("add-course-span") as span:
         span_attributes(span)
         if request.method == 'POST':
             s_time = time.time_ns()
-            course = {
-                'code': request.form['code'],
-                'name': request.form['name'],
-                'instructor': request.form['instructor'],
-                'semester': request.form['semester'],
-                'schedule': request.form['schedule'],
-                'classroom': request.form['classroom'],
-                'prerequisites': request.form['prerequisites'],
-                'grading': request.form['grading'],
-                'description': request.form['description']
-            }
-
-            span.add_event("Form Submissions Recorded")
-
-            # form validation
-            if any(course[key].strip() == '' for key in list(course.keys())):
-                flash(f"You have left some fields empty, Please fill up!!")
-                span.add_event("Malformed Form Submissions")
+            course = {key: request.form[key] for key in request.form}
+            if any(value.strip() == '' for value in course.values()):
+                flash("Please fill all required fields", "warning")
                 span.set_status(StatusCode.ERROR)
-                span.set_attribute("http.status_code", 400)
-                logger.warn("form_submission_warning: missing_required_fields")
                 error_counter.add(1)
                 return render_template('add_course.html')
 
-            span.add_event("Submissions Validated Successfully")
             save_courses(course)
-            span.add_event("New Course Added: " + course['code'] + ' ' + course['name'])
             flash(f"Course '{course['name']}' added successfully!", "success")
-            t_time = time.time_ns() - s_time
-            timing_histogram.record(t_time, {"page": "add-course"})
-            new_course_counter.add(1)
             logger.info(f"Course Added: {course['code']} {course['name']}")
-
-            span.set_status(StatusCode.OK)
-            span.set_attribute("http.status_code", 200)
             return redirect(url_for('course_catalog'))
 
-        response = render_template('add_course.html')
-        span.add_event("Rendered Add Course page")
-        span.set_status(StatusCode.OK)
-        span.set_attribute("http.status_code", 200)
         logger.info("Page Rendered: Add Course")
-        return response, 200
+        return render_template('add_course.html')
 
 
 @app.route('/course/<code>')
 def course_details(code):
+    """displays the details of a specific course"""
     with create_span("course-details-span") as span:
         span_attributes(span)
         courses = load_courses()
-
         course = next((course for course in courses if course['code'] == code), None)
         if not course:
-            span.add_event("Requested Course " + code + " Not found")
-            flash(f"No course found with code '{code}'.", "error")
-            span.set_status(StatusCode.ERROR)
-            span.set_attribute("http.status_code", 400)
+            flash(f"No course found with code '{code}'", "error")
             logger.error(f"Course {code} Not Found")
             return redirect(url_for('course_catalog'))
 
-        response = render_template('course_details.html', course=course)
-        span.add_event("Requested Course " + code + " details rendered")
-
-        span.set_status(StatusCode.OK)
-        span.set_attribute("http.status_code", 200)
-        logger.info("Page Rendered: Course Details")
-        return response, 200
-
+        logger.info(f"Page Rendered: Course Details - {code}")
+        return render_template('course_details.html', course=course)
 
 @app.route("/manual-trace")
 def manual_trace():
